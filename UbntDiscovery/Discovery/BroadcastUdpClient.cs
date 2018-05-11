077c226759fc2c7b806b1abd03a351d21d2347a5
willmackay <william.mackay@alyrica.net>
2018-05-11 11:28:57 -0700

    discover on all network interfaces, not just the main one

---
+++ UbntDiscovery/Discovery/BroadcastUdpClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace UbntDiscovery
+{
+    class BroadcastUdpClient : UdpClient
+    {
+        public BroadcastUdpClient() : base()
+        {
+            //Calls the protected Client property belonging to the UdpClient base class.
+            Socket s = this.Client;
+            //Uses the Socket returned by Client to set an option that is not available using UdpClient.
+            s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontRoute, 1);
+        }
+
+        public BroadcastUdpClient(IPEndPoint ipLocalEndPoint) : base(ipLocalEndPoint)
+        {
+            //Calls the protected Client property belonging to the UdpClient base class.
+            Socket s = this.Client;
+            //Uses the Socket returned by Client to set an option that is not available using UdpClient.
+            s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+            s.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.DontRoute, 1);
+        }
+    }
+}
